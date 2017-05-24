43ecd9432b6acc62a16314632bec29ea9197edd6
ASTM <astm_desig@hotmail.com>
2017-05-24 20:20:36 +0300

    Lynda Exercise Files

---
+++ 01_03/finished/angular/js/controllers.js
@@ -0,0 +1,8 @@
+var myNameSpace=angular.module('myApp',[]);
+myNameSpace.controller('MyController',function MyController($scope){
+   $scope.author={
+      'name':'ahmed',
+      'title':'Developer'.
+      'company':'naqla'
+  }
+});
